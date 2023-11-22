eabc6ef485db3497741932672fdbe152934e6de6
jhormanRoux <124332855+jhormanRoux@users.noreply.github.com>
2023-11-22 10:01:46 -0500

    añadidos todos los trailers

--- app.js
+++ app.js
@@ -60,4 +60,18 @@ fila.addEventListener('mouseleave', () => {
 });
 
 
+// ? ----- ----- Modal ----- -----
 
+function openModal(videoId) {
+    var modal = document.getElementById("myModal");
+    var video = document.getElementById("video");
+    video.src = "https://www.youtube.com/embed/" + videoId;
+    modal.style.display = "block";
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    var video = document.getElementById("video");
+    video.src = "";
+    modal.style.display = "none";
+  }
